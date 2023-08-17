9bbe1cc1ce484357f4cbed5280a0bdecf00b1746
JessicaCaiqingYang <caiqingyang28@gmail.com>
2023-08-17 01:24:27 +0100

    added solution to calculate gas cost

--- transaction.py
+++ transaction.py
@@ -73,6 +73,21 @@ with open('transactions.csv') as f:
         )
         transactions.append(transaction)
 
-# Access execution_timestamp
+
+import requests
+
+GWEI_TO_ETH = 1e-9
+# Loop through transactions
 for transaction in transactions:
-    print(transaction.execution_timestamp)
+    # Get ETH price in dollars at execution timestamp
+    eth_price_url = f"https://api.coingecko.com/api/v3/coins/ethereum/history?from_timestamp={int(transaction.execution_timestamp.timestamp())}"
+
+    eth_price_resp = requests.get(eth_price_url)
+
+    eth_price = eth_price_resp.json()['market_data']['current_price']['usd']
+
+    # Calculate gas cost in dollars
+    gas_cost_dollars = transaction.gas * transaction.gas_price * GWEI_TO_ETH * eth_price
+
+    # Add gas cost to transaction
+    transaction.gas_cost_dollars = gas_cost_dollars
